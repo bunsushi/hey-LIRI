157b2ca7a8aee3e1f4465071802930a4971a08e1
bunsushi <tbunsu@gmail.com>
2018-04-09 10:37:17 -0400

    fetch 20 most recent tweets = default

--- liri.js
+++ liri.js
@@ -1,5 +1,16 @@
-require("dotenv").config();
+require('dotenv').config();
 var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var params = { screen_name: 'mesobotamia' };
+client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    if (!error) {
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+        }
+    }
+});
