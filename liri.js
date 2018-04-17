0b1116e47240026273f883d572a172b3c4e0bea4
bunsushi <tbunsu@gmail.com>
2018-04-16 21:00:01 -0400

    log node app commands to log.txt

--- liri.js
+++ liri.js
@@ -35,13 +35,22 @@ function myTweets(username) {
     var params = { screen_name: username };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
+            dateStamp();
             for (var i = 0; i < tweets.length; i++) {
-                console.log("\n" + (i + 1) + ") " + tweets[i].text);
-                console.log(moment(tweets[i].created_at, "ddd MMM DD HH:mm:ss Z YYYY").format('LLLL'));
-            }
-        }
+                var tweetsText =
+                    "\n" + (i + 1) + ") " + tweets[i].text +
+                    "\n" + moment(tweets[i].created_at, "ddd MMM DD HH:mm:ss Z YYYY").format('LLLL');
+                console.log(tweetsText);
+
+                fs.appendFile("log.txt", tweetsText, function (err, data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
+            };
+        };
     });
-}
+};
 
 function spotifyThisSong() {
     console.log("Searching for that song...");
@@ -54,15 +63,22 @@ function spotifyThisSong() {
             var info = data.tracks.items;
             for (var i = 0; i < info.length; i++) {
                 if (info[i].album.name === "The Sign (US Album) [Remastered]" && info[i].name === "The Sign") {
-                    console.log(
+                    var theSign =
                         "\nArtist: " + info[i].artists[0].name +
                         "\nSong: " + info[i].name +
                         "\nAlbum: " + info[i].album.name +
                         // ATTN: Some songs preview url returns null
                         "\nPreview: " + info[i].preview_url
-                    );
-                }
-            }
+                    console.log(theSign);
+
+                    dateStamp();
+                    fs.appendFile("log.txt", theSign, function (err, data) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                    })
+                };
+            };
         });
     }
     else if (media) {
@@ -71,13 +87,22 @@ function spotifyThisSong() {
                 return console.log('Error occurred: ' + err);
             }
             var info = data.tracks.items;
-            console.log(
+
+            var songData =
                 "\nArtist: " + info[0].artists[0].name +
                 "\nSong: " + info[0].name +
                 "\nAlbum: " + info[0].album.name +
                 // ATTN: Some songs preview url returns null
                 "\nPreview: " + info[0].preview_url
-            )
+
+            console.log(songData);
+
+            dateStamp();
+            fs.appendFile("log.txt", songData, function (err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
         });
     };
 };
@@ -91,15 +116,21 @@ function movieThis() {
         request(queryUrl, function (error, response, body) {
             // If the request is successful
             if (!error && response.statusCode === 200) {
-                console.log(
+                var mrNobody =
                     "\nTitle: " + JSON.parse(body).Title +
                     "\nRelease Year: " + JSON.parse(body).Year +
                     "\nIMDB Rating: " + JSON.parse(body).Ratings[0].Value +
                     "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
                     "\nCountry: " + JSON.parse(body).Country +
                     "\nStarring: " + JSON.parse(body).Actors
-                );
+                console.log(mrNobody);
             }
+            dateStamp();
+            fs.appendFile("log.txt", mrNobody, function (err, data) {
+                if (err) {
+                    return console.log(err);
+                };
+            });
         });
     }
     else if (media) {
@@ -108,15 +139,21 @@ function movieThis() {
         request(queryUrl, function (error, response, body) {
             // If the request is successful
             if (!error && response.statusCode === 200) {
-                console.log(
+                var movieData =
                     "\nTitle: " + JSON.parse(body).Title +
                     "\nRelease Year: " + JSON.parse(body).Year +
                     "\nIMDB Rating: " + JSON.parse(body).Ratings[0].Value +
                     "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
                     "\nCountry: " + JSON.parse(body).Country +
                     "\nStarring: " + JSON.parse(body).Actors
-                );
-            }
+                console.log(movieData);
+            };
+            dateStamp();
+            fs.appendFile("log.txt", movieData, function (err, data) {
+                if (err) {
+                    return console.log(err);
+                };
+            });
         });
     };
 };
@@ -140,6 +177,21 @@ function justDoIt() {
         exec(cmd, function (error, stdout, stderr) {
             // command output is in stdout
             console.log(stdout);
+            dateStamp();
+            fs.appendFile("log.txt", stdout, function (err, data) {
+                if (err) {
+                    return console.log(err);
+                };
+            });
         });
+    });
+};
+
+function dateStamp() {
+    var lineBreak = '\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' + '\nDatestamp: ' + moment().format('MMMM Do YYYY, h:mm:ss a') + '\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%';
+    fs.appendFile("log.txt", lineBreak, function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
     });
 }
